d61819998d4a8b90f47bc60dbe343d7acc214437
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2023-11-12 20:27:13 -0500

    Fix lint of bash code

--- buildRPiZeroCrossCompiler.sh
+++ buildRPiZeroCrossCompiler.sh
@@ -4,6 +4,7 @@ TIME_START=$(date +%s)
 
 WORKING_DIR=$(pwd)
 
+# shellcheck source=build-rpi-gcc-config
 source build-rpi-gcc-config
 
 
@@ -19,34 +20,34 @@ makeDir()
 
 # Temporarily change ownership of destination (do this early so script can run unattended until end)
 echo "Temporarily changing ownership of destination directory to $USER..."
-sudo chown -R $USER $PREFIX
+sudo chown -R "$USER" "$PREFIX"
 
 
 
 
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 echo "Downloading and expanding $NAME_BINUTILS (if needed)..."
-if [ ! -d $NAME_BINUTILS ]; then
-    if [ ! -f $NAME_BINUTILS.tar.bz2 ]; then
+if [ ! -d "$NAME_BINUTILS" ]; then
+    if [ ! -f "$NAME_BINUTILS".tar.bz2 ]; then
         echo "Downloading $NAME_BINUTILS..."
-        wget https://ftpmirror.gnu.org/binutils/$NAME_BINUTILS.tar.bz2
+        wget https://ftpmirror.gnu.org/binutils/"$NAME_BINUTILS".tar.bz2
     fi
     echo "Extracting $NAME_BINUTILS..."
-    tar xf $NAME_BINUTILS.tar.bz2
+    tar xf "$NAME_BINUTILS".tar.bz2
 else
     echo "$NAME_BINUTILS already present"
 fi
 
 
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 echo "Downloading and expanding $NAME_GCC (if needed)..."
-if [ ! -d $NAME_GCC ]; then
-    if [ ! -f $NAME_GCC.tar.gz ]; then
+if [ ! -d "$NAME_GCC" ]; then
+    if [ ! -f "$NAME_GCC".tar.gz ]; then
         echo "Downloading $NAME_GCC..."
-        wget https://ftpmirror.gnu.org/gcc/$NAME_GCC/$NAME_GCC.tar.gz
+        wget https://ftpmirror.gnu.org/gcc/"$NAME_GCC"/"$NAME_GCC".tar.gz
     fi
     echo "Extracting $NAME_GCC..."
-    tar xf $NAME_GCC.tar.gz
+    tar xf "$NAME_GCC".tar.gz
 
     echo "Patching ${NAME_GCC}//libsanitizer/asan/asan_linux.cpp..."
     VER10PATCH="-10"
@@ -55,36 +56,36 @@ if [ ! -d $NAME_GCC ]; then
     if [[ $NAME_GCC =~ $VER10PATCH ]] || [[ $NAME_GCC =~ $VER11PATCH ]] || [[ $NAME_GCC =~ $VER12PATCH ]]
     then
         echo "Patching ${NAME_GCC}/libsanitizer/asan/asan_linux.cpp..."
-        cd $WORKING_DIR
-        cd ${NAME_GCC}/libsanitizer/asan/
+        cd "$WORKING_DIR"
+        cd "$NAME_GCC"/libsanitizer/asan/
         cp asan_linux.cpp asan_linux.cpp.orig
-        patch < ${WORKING_DIR}/asan_linux.patch
+        patch < "$WORKING_DIR"/asan_linux.patch
     fi
 
     echo "Downloading prerequisites for GCC..."
-    cd $WORKING_DIR
-    cd $NAME_GCC
+    cd "$WORKING_DIR"
+    cd "$NAME_GCC"
     contrib/download_prerequisites
 else
     echo "$NAME_GCC already present"
 fi
 
 
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 echo "Downloading and expanding $NAME_GLIBC (if needed)..."
-if [ ! -d $NAME_GLIBC ]; then
-    if [ ! -f $NAME_GLIBC.tar.bz2 ]; then
+if [ ! -d "$NAME_GLIBC" ]; then
+    if [ ! -f "$NAME_GLIBC".tar.bz2 ]; then
         echo "Downloading $NAME_GLIBC..."
-        wget https://ftpmirror.gnu.org/glibc/$NAME_GLIBC.tar.bz2
+        wget https://ftpmirror.gnu.org/glibc/"$NAME_GLIBC".tar.bz2
     fi
     echo "Extracting $NAME_GLIBC..."
-    tar xf $NAME_GLIBC.tar.bz2
+    tar xf "$NAME_GLIBC".tar.bz2
 else
     echo "$NAME_GLIBC already present"
 fi
 
 
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 if [ -d linux ]; then
     echo "Updating RPi Linux headers..."
     cd linux
@@ -96,20 +97,19 @@ fi
 
 
 echo "Copying kernel headers to cross-compiler destination folder..."
-cd $WORKING_DIR
-cd linux
+cd "$WORKING_DIR/linux"
 export KERNEL=$KERNELNEEDED
-make ARCH=arm INSTALL_HDR_PATH=$PREFIX/arm-linux-gnueabihf headers_install
+make ARCH=arm INSTALL_HDR_PATH="$PREFIX"/arm-linux-gnueabihf headers_install
 
 
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 echo "Building binutils..."
-NAME_BINUTILS_BLD=${NAME_BINUTILS}_build
-makeDir $NAME_BINUTILS_BLD
-cd $NAME_BINUTILS_BLD
-../$NAME_BINUTILS/configure --prefix=$PREFIX --target=arm-linux-gnueabihf --with-arch=$ARMARCH \
+NAME_BINUTILS_BLD="$NAME_BINUTILS"_build
+makeDir "$NAME_BINUTILS_BLD"
+cd "$NAME_BINUTILS_BLD"
+../"$NAME_BINUTILS"/configure --prefix="$PREFIX" --target=arm-linux-gnueabihf --with-arch="$ARMARCH" \
     --with-fpu=vfp --with-float=hard --disable-multilib
-make -j $JOBS
+make -j "$JOBS"
 make install
 
 
@@ -118,57 +118,54 @@ echo "Building GCC and GLIBC in stages..."
 
 
 echo "Stage 1: Partial build of GCC..."
-cd $WORKING_DIR
-NAME_GCC_BLD=${NAME_GCC}_build
-makeDir $NAME_GCC_BLD
-cd $NAME_GCC_BLD
-../$NAME_GCC/configure --prefix=$PREFIX --target=arm-linux-gnueabihf --enable-languages=c,c++ \
-    --with-arch=$ARMARCH --with-fpu=vfp --with-float=hard --disable-multilib
-make -j $JOBS all-gcc
+cd "$WORKING_DIR"
+NAME_GCC_BLD="$NAME_GCC"_build
+makeDir "$NAME_GCC_BLD"
+cd "$NAME_GCC_BLD"
+../"$NAME_GCC"/configure --prefix="$PREFIX" --target=arm-linux-gnueabihf --enable-languages=c,c++ \
+    --with-arch="$ARMARCH" --with-fpu=vfp --with-float=hard --disable-multilib
+make -j "$JOBS" all-gcc
 make install-gcc
 
 
 echo "Stage 2: Partial build of GLIBC..."
-cd $WORKING_DIR
-NAME_GLIBC_BLD=${NAME_GLIBC}_build
-makeDir $NAME_GLIBC_BLD
-cd $NAME_GLIBC_BLD
-../$NAME_GLIBC/configure --prefix=$PREFIX/arm-linux-gnueabihf --build=$MACHTYPE --host=arm-linux-gnueabihf \
-    --target=arm-linux-gnueabihf --with-arch=$ARMARCH --with-fpu=vfp --with-float=hard \
-    --with-headers=$PREFIX/arm-linux-gnueabihf/include --disable-multilib libc_cv_forced_unwind=yes
+cd "$WORKING_DIR"
+NAME_GLIBC_BLD="$NAME_GLIBC"_build
+makeDir "$NAME_GLIBC_BLD"
+cd "$NAME_GLIBC_BLD"
+../"$NAME_GLIBC"/configure --prefix="$PREFIX"/arm-linux-gnueabihf --build="$MACHTYPE" --host=arm-linux-gnueabihf \
+    --target=arm-linux-gnueabihf --with-arch="$ARMARCH" --with-fpu=vfp --with-float=hard \
+    --with-headers="$PREFIX"/arm-linux-gnueabihf/include --disable-multilib libc_cv_forced_unwind=yes
 make install-bootstrap-headers=yes install-headers
-make -j $JOBS csu/subdir_lib
-install csu/crt1.o csu/crti.o csu/crtn.o $PREFIX/arm-linux-gnueabihf/lib
-arm-linux-gnueabihf-gcc -nostdlib -nostartfiles -shared -x c /dev/null -o $PREFIX/arm-linux-gnueabihf/lib/libc.so
-touch $PREFIX/arm-linux-gnueabihf/include/gnu/stubs.h
+make -j "$JOBS" csu/subdir_lib
+install csu/crt1.o csu/crti.o csu/crtn.o "$PREFIX"/arm-linux-gnueabihf/lib
+arm-linux-gnueabihf-gcc -nostdlib -nostartfiles -shared -x c /dev/null -o "$PREFIX"/arm-linux-gnueabihf/lib/libc.so
+touch "$PREFIX"/arm-linux-gnueabihf/include/gnu/stubs.h
 
 
 echo "Stage 3: Continue partial build of GCC..."
-cd $WORKING_DIR
-cd $NAME_GCC_BLD
-make -j $JOBS  all-target-libgcc
+cd "$WORKING_DIR/$NAME_GCC_BLD"
+make -j "$JOBS"  all-target-libgcc
 make install-target-libgcc
 
 
 echo "Stage 4: Finish build of GLIBC..."
-cd $WORKING_DIR
-cd $NAME_GLIBC_BLD
-make -j $JOBS
+cd "$WORKING_DIR/$NAME_GLIBC_BLD"
+make -j "$JOBS"
 make install
 
 
 echo "Stage 5: Finish build of GCC..."
-cd $WORKING_DIR
-cd $NAME_GCC_BLD
-make -j $JOBS
+cd "$WORKING_DIR/$NAME_GCC_BLD"
+make -j "$JOBS"
 make install
 
 
 echo "Build finished, restoring ownership of destination files to root..."
-sudo chown -R root:root $PREFIX 
+sudo chown -R root:root "$PREFIX" 
 
 TIME_END=$(date +%s)
-TIME_RUN=$(($TIME_END - $TIME_START))
+TIME_RUN=$((TIME_END - TIME_START))
 
 echo ""
 echo "Elapsed time $TIME_RUN seconds"
